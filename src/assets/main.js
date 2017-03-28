50d92dcaedfc740d008b5155a82b2a37c4c2a3f0
noam sha <noam sha>
2017-03-28 21:36:42 +0300

    Implemented Ajax to get and show all badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: "https://www.codeschool.com/users/Shavvy.json",
+    dataType: "jsonp",
+    success: function(data){
+      var completed = data["courses"]["completed"];
+      
+      completed.forEach(function(element){
+        //console.log(element);
+        $("#badges")
+          .append(`
+          <div class='course'>
+            <h3>` + element.title + `</h3>
+            <img src= ` + element.badge + `></img>
+            <a href=` + element.url + ` target='_blank' class='btn btn-primary'>See Course</a>
+          </div>`)
+      });
+    }
+  })
+  
 });
